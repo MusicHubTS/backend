5f0c3d6b96debe259ef24d8d8d1ca12e6f4a1f05
LittleYe233 <30514318+LittleYe233@users.noreply.github.com>
2022-05-30 15:57:20 +0800

    feat: initialize with Ts.ED

---
+++ src/Server.integration.spec.ts
@@ -0,0 +1,25 @@
+import { PlatformTest } from "@tsed/common";
+import SuperTest from "supertest";
+import { Server } from "./Server";
+
+describe("Server", () => {
+  let request: SuperTest.SuperTest<SuperTest.Test>;
+
+  beforeEach(PlatformTest.bootstrap(Server));
+  beforeEach(() => {
+    request = SuperTest(PlatformTest.callback());
+  });
+
+  afterEach(PlatformTest.reset);
+
+  it("should call GET /rest", async () => {
+     const response = await request.get("/rest").expect(404);
+
+     expect(response.body).toEqual({
+       errors: [],
+       message: 'Resource "/rest" not found',
+       name: "NOT_FOUND",
+       status: 404,
+     });
+  });
+});
