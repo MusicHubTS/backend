4b5af9018f51e414970fff259316e10d1d920c38
LittleYe233 <30514318+LittleYe233@users.noreply.github.com>
2022-05-31 00:32:06 +0800

    chore: watch `typings/` folder
    `typings` folder has been removed from `.gitignore` because TypeScript
    cannot detect `*.d.ts` files with the same name of `*.ts`. The detailed
    information can be seen in
    https://github.com/typescript-eslint/typescript-eslint/issues/2811.
    So we should try to use the custom typing folder.

---
+++ typings/src/config/project/index.d.ts
@@ -0,0 +1,28 @@
+/**
+ * Declaration of interface for one piece of songs.
+ * 
+ * @param name Name of the song.
+ * @param artist Array of the artists.
+ * @param path Path of the media file.
+ * @param cover Path of the album cover image.
+ * @param lrc Path of the lyric file (should be SRT).
+ */
+export interface SongPiece {
+  name: string,
+  artist?: string[],
+  path: string,
+  cover?: string,
+  lrc?: string
+}
+
+export interface ProjectConfig {
+  server?: {
+    owner?: string,
+    root?: string,
+    cdn?: string | Record<string, string>
+  },
+  songs?: {
+    prefix?: string,
+    pieces?: Record<string, SongPiece>[]
+  }
+}
