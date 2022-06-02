3a1e8d82624109bd4fc311b118156c138e724759
LittleYe233 <30514318+LittleYe233@users.noreply.github.com>
2022-06-02 21:51:55 +0800

    chore: rename `*.d.ts` to `*.ts`
    It seems that TypeScript compiler doesn't recognize `*.d.ts` in **this**
    **project** (working in another repo of mine). So we renamed it.
    
    See: 4b5af9018f51e414970fff259316e10d1d920c38

--- typings/src/config/project/index.ts
+++ typings/src/config/project/index.ts
@@ -1,6 +1,6 @@
 /**
  * Declaration of interface for one piece of songs.
- * 
+ *
  * @param name Name of the song.
  * @param artist Array of the artists.
  * @param path Path of the media file.
@@ -8,21 +8,21 @@
  * @param lrc Path of the lyric file (should be SRT).
  */
 export interface SongPiece {
-  name: string,
-  artist?: string[],
-  path: string,
-  cover?: string,
-  lrc?: string
+  name: string;
+  artist?: string[];
+  path: string;
+  cover?: string;
+  lrc?: string;
 }
 
 export interface ProjectConfig {
   server?: {
-    owner?: string,
-    root?: string,
-    cdn?: string | Record<string, string>
-  },
+    owner?: string;
+    root?: string;
+    cdn?: string | Record<string, string>;
+  };
   songs?: {
-    prefix?: string,
-    pieces?: Record<string, SongPiece>[]
-  }
+    prefix?: string;
+    pieces?: Record<string, SongPiece>;
+  };
 }
