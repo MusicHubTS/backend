9563a93943fc86240a45ac3b7b3c01384feb4454
LittleYe233 <30514318+LittleYe233@users.noreply.github.com>
2022-06-03 17:03:10 +0800

    chore: change the pattern of returns of song config API

--- src/controllers/rest/get_config/SongsController.ts
+++ src/controllers/rest/get_config/SongsController.ts
@@ -1,6 +1,19 @@
 import {Controller, Constant} from '@tsed/di';
 import {Get, Returns} from '@tsed/schema';
 import { ProjectConfig } from 'typings/src/config/project';
+import { APlayerAudios } from 'typings/src/controllers/rest/get_config';
+import * as path from 'path';
+
+function parseSongs(songs: ProjectConfig['songs']): APlayerAudios {
+  const { prefix, pieces } = songs;
+  return Object.values(pieces).map(p => ({
+    name: p.name,
+    artist: typeof(p.artist) === 'string' ? p.artist : p.artist.join(','),
+    url: path.join(prefix, p.path),
+    cover: p.cover === null ? null : path.join(prefix, p.cover),
+    lrc: p.lrc === null ? null : path.join(prefix, p.lrc)
+  }));
+}
 
 @Controller('/songs')
 export class SongsController {
@@ -11,6 +24,6 @@ export class SongsController {
   @Get('/')
   @(Returns(200, String).ContentType('application/json; charset=utf-8'))
   get() {
-    return JSON.stringify(this.projectConfig.default.songs);
-  }
+    return JSON.stringify(parseSongs(this.projectConfig.default.songs));
+  }                              
 }
