eb4e740a747719eeb876226bd61a5af8788f017e
LittleYe233 <30514318+LittleYe233@users.noreply.github.com>
2022-06-08 08:57:02 +0800

    chore: standardize project config
    This commit introduces a new function `standardize()` to pre-fill a
    default value (e.g. `null`) to missing fields and refactor its
    structure.
    
    Note that it still needs more standardization on other fields like CDN
    settings.

--- src/config/project/default.ts
+++ src/config/project/default.ts
@@ -1,3 +1,5 @@
+import { SONG_PIECE_KEYS } from 'typings';
+
 export const DEFAULT_CONFIG = {
   server: {
     owner: 'Owner',
@@ -9,3 +11,10 @@ export const DEFAULT_CONFIG = {
     pieces: []
   }
 };
+
+// In Python: DEFAULT_SONG_PIECE = {v: None for v in SONG_PIECE_KEYS}
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export const DEFAULT_SONG_PIECE = SONG_PIECE_KEYS.reduce((obj: any, v) => {
+  obj[v] = null;
+  return obj;
+}, {});
--- src/config/project/index.ts
+++ src/config/project/index.ts
@@ -3,16 +3,34 @@ import { $log } from '@tsed/logger';
 import { merge } from 'lodash';
 import YAML from 'yaml';
 import { ProjectConfig } from 'typings';
-import { DEFAULT_CONFIG } from './default';
+import { DEFAULT_CONFIG, DEFAULT_SONG_PIECE } from './default';
+
+function standardize(cfg: ProjectConfig): ProjectConfig {
+  // Step 1: Merge with a rough framework
+  const standardized: ProjectConfig = merge(DEFAULT_CONFIG, cfg);
+
+  // Step 2: Standardize song pieces
+  /**
+   * @note Don't use `merge()` here, for it will modify the destination object in-place.
+   */
+  standardized.songs.pieces.forEach((v, idx, arr) => {
+    arr[idx] = { ...DEFAULT_SONG_PIECE, ...v };
+  });
+
+  return standardized;
+}
 
 let cfg: ProjectConfig;
 try {
-  cfg = merge(DEFAULT_CONFIG, YAML.parse(readFileSync('./config.yml', { encoding: 'utf8' })));
+  cfg = YAML.parse(readFileSync('./config.yml', { encoding: 'utf8' }));
 } catch (e) {
   $log.warn('Failed to read YAML configurations from `./config.yml`.');
   cfg = DEFAULT_CONFIG;
 }
+const standardized = standardize(cfg);
 $log.debug('Configurations:');
-$log.debug(JSON.stringify(cfg));
+$log.debug(JSON.stringify(cfg, undefined, 2));  // original config, unless parsing has any error
+$log.debug('Standardized configuration:');
+$log.debug(JSON.stringify(standardized, undefined, 2));  // standardized form
 
 export default cfg;
